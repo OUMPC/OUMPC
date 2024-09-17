4228f085930d3dc5fe1a60adee7ce8c7eb1aadd0
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-09-17 23:48:56 +0700

    1709

---
+++ src/js/home.js
@@ -0,0 +1,210 @@
+const slogans = ["Nơi tình thân và đam mê kết nối", "while (alive) { code(); sleep(); eat();)"];
+const slogan = document.querySelector('.title p');
+document.INTERVAL = {
+    caption: null
+}
+
+initPage();
+initQA();
+runCaption();
+
+
+
+async function initQA(){
+    await fetch('./src/data/qa.json')
+    .then(res => res.json()).then(data => {
+        const questions = document.querySelector('.questions');
+        questions.innerHTML = ""
+        data.forEach((e,i)=>{
+            questions.innerHTML += `
+            <li>
+                <div class="ask">
+                <span>${i+1 +". "+e.Q}</span>
+                <i class="fa-solid fa-chevron-up"></i>
+                </div>
+                <div class="reply">
+                    <p>${e.A}</p>
+                </div>
+            </li>
+            `
+        })
+    })
+    .catch(err => console.log(err))
+    const ask = document.querySelectorAll('.questions li');
+
+    ask.forEach((item) => {
+        item.addEventListener('click', () => {            
+            ask.forEach((i) => {
+                if(i !== item) {
+                    i.classList.remove('active');
+                }
+            })
+            item.classList.toggle('active');
+        })
+    })
+}
+
+async function initPage(){
+    fetch('./src/data/infoMember.json').then(res => res.json())
+    .then(data => {
+        const coVan = document.querySelector('#coVan');
+        coVan.innerHTML = ""
+        data.coVan.forEach((e,i)=>{
+            coVan.innerHTML += `
+            <li>
+                <div class="img">
+                    <img src="${e.avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+                </div>
+                <div class="info">
+                    <span>${e.call}</span>
+                    <h2>${e.name}</h2>
+                    <p>${e.bio}</p>
+                </div>
+            </li>
+            `
+        })
+        
+
+        const chuNhiem = document.querySelector('#chuNhiem');
+        chuNhiem.innerHTML= `
+        <div class="box">
+            <div class="img">
+                <img src="${data.chuNhiem[1].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.chuNhiem[1].name || "Thành viên thứ 2"}</h2>
+                <p>Phó chủ nhiệm</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="${data.chuNhiem[0].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.chuNhiem[0].name || "Thành viên thứ 1"}</h2>
+                <p>Chủ nhiệm</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="${data.chuNhiem[2].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.chuNhiem[2].name || "Thành viên thứ 3"}</h2>
+                <p>Phó chủ nhiệm</p>
+            </div>
+        </div>
+        `
+        const banLT = document.querySelector('#banLT');
+        banLT.innerHTML= `
+        <div class="box">
+            <div class="img">
+                <img src="${data.lapTrinh[0].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.lapTrinh[0].name || "Thành viên thứ 1"}</h2>
+                <p>Trưởng ban lập trình</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="${data.lapTrinh[1].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.lapTrinh[1].name || "Thành viên thứ 2"}</h2>
+                <p>Phó ban lập trình</p>
+            </div>
+        </div>
+        `
+
+        const banHC = document.querySelector('#banHC');
+        banHC.innerHTML= `
+        <div class="box">
+            <div class="img">
+                <img src="${data.hauCan[0].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.hauCan[0].name || "Thành viên thứ 1"}</h2>
+                <p>Trưởng ban hậu cần</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="${data.hauCan[1].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.hauCan[1].name || "Thành viên thứ 2"}</h2>
+                <p>Phó ban hậu cần</p>
+            </div>
+        </div>
+        `
+
+        const banTT = document.querySelector('#banTT');
+        banTT.innerHTML= `
+        <div class="box">
+            <div class="img">
+                <img src="${data.truyenThong[0].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.truyenThong[0].name || "Thành viên thứ 1"}</h2>
+                <p>Trưởng ban truyền thông</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="${data.truyenThong[1].avatar || "./src/asset/image/members/member.png"}" alt="" srcset="">
+            </div>
+            <div class="info">
+                <h2>${data.truyenThong[1].name || "Thành viên thứ 2"}</h2>
+                <p>Phó ban truyền thông</p>
+            </div>
+        </div>
+        `
+        
+
+    })
+    .catch(err => console.log(err))
+}
+
+
+
+function runCaption() {
+    let time = 0
+    let title = ""
+    const caption = slogans[Math.floor(Math.random() * slogans.length)]
+    const captionSliced = chunkString(caption,parseInt(caption.length / 20))
+    document.INTERVAL.caption  = setInterval(()=>{
+        if (captionSliced.length!=0) {
+            title = title + captionSliced[0]
+            captionSliced.shift()
+            slogan.innerText = (title + "_")
+            return 
+        }
+        time+=80
+        if (time >= 5000) {
+            if (title=="") {
+                clearInterval(document.INTERVAL.caption)
+                runCaption()
+            }
+            title = title.substring(0,title.length - 2 <0?0:title.length - 5)
+            slogan.innerText = (title + "_")
+        }
+        if (time % 320 == 0) {
+            slogan.innerText = (title + " ")
+            return
+        }
+
+        if (time % 160 == 0) {
+            slogan.innerText = (title + "_")
+        }
+        
+    },80)
+}
+
+function chunkString(str, chunkSize) {
+    const result = [];
+    for (let i = 0; i < str.length; i += chunkSize) {
+      result.push(str.slice(i, i + chunkSize));
+    }
+    return result;
+}
---
+++ src/js/init.js
@@ -0,0 +1,168 @@
+const headerHTML = `
+<nav>
+    <div class="row-l">
+        <a class="logo" href="./">
+            <img src="./src/asset/image/logo/logo.png" alt="OUMPC">
+            <span>
+                OUMPC
+            </span>
+        </a>
+    </div>
+    <div class="row-r">
+        <div id="nav-toggle" >
+            <i class="fa-solid fa-bars"></i>
+        </div>
+        <div class="menu">
+            <a class="logo" href="#">
+                <img src="./src/asset/image/logo/logo.png" alt="OUMPC">
+                <span>
+                    OUMPC
+                </span>
+            </a>
+            <ul>
+                <li>
+                    <a href="./">Trang chủ</a>
+                </li>
+                <li>
+                    <a href="./daotao.html">Đào tạo</a>
+                </li>
+                <li>
+                    <a href="./sukien.html">Sự kiện</a>
+                </li>
+                <li>
+                    <a href="./thanhtuu.html">Thành tựu</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+`
+const footerHTML = `
+<div class="left">
+    <div class="logo">
+        <img src="./src/asset/image/logo/logo.png" alt="">
+    </div>
+    <p>Mobile Programming Club</p>
+    
+</div>
+<div class="mid">
+    <p><b>Tìm hiểu thêm</b></p>
+    <p>
+        <a href="#">Về chúng tôi</a>
+    </p>
+    <p>
+        <a href="#">Liên hệ</a>
+    </p>
+</div>
+<div cla    ss="right">
+    <p><b>Liên hệ</b></p>
+    <p>
+        <a href="mailto:it.mpclub@ou.edu.vn">it.mpclub@ou.edu.vn</a>
+    </p>
+    <p>
+        <a href="https://ou.edu.vn/">Trường Đại học Mở Tp.HCM</a>
+    </p>
+    <p>
+        <a href="http://it.ou.edu.vn/">Khoa Công nghệ thông tin</a>
+    </p>
+</div>
+<div class="copy">
+    <p>© 2021 Mobile Programming Club</p>
+</div>
+`
+
+
+window.onload = ()=>{
+    initCSS()
+    initHeader()   
+    initFooter() 
+}
+
+
+
+window.onscroll = (e) => {
+    const header = document.querySelector('header');
+    const up = document.querySelector('#up')
+    if(window.scrollY > 10){
+        header.classList.add('scroll');
+    }else{
+        header.classList.remove('scroll');
+    }
+
+    if (window.scrollY > 1000) {
+        up.classList.add('active');
+    } else {
+        up.classList.remove('active');
+    }
+
+    up.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
+
+function initCSS(){
+    const linkAWS = document.createElement('link');
+    linkAWS.rel = 'stylesheet';
+    linkAWS.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css';
+    linkAWS.integrity = 'sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==';
+    linkAWS.crossOrigin = 'anonymous';
+    
+
+    const linkInit = document.createElement('link');
+    linkInit.rel = 'stylesheet';
+    linkInit.href = './src/css/init.css';
+
+    const style = document.createElement('style');
+    style.textContent = "@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');";
+
+    const favicon = document.createElement('link');
+    favicon.rel = 'icon';
+    favicon.href = './src/asset/image/logo/logo.png';
+
+    const animateCSS = document.createElement('link');
+    animateCSS.rel = 'stylesheet';
+    animateCSS.href = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
+    
+
+    document.head.appendChild(favicon);
+    document.head.appendChild(style);
+    document.head.appendChild(linkAWS);
+    document.head.appendChild(linkInit);
+    document.head.appendChild(animateCSS);
+}
+
+function initHeader(){
+    const header = document.querySelector('header');
+    header.innerHTML = headerHTML;
+
+    const navToggle = document.getElementById('nav-toggle');
+    const menu = document.querySelector('.menu');
+
+    navToggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+
+    const menuItems = document.querySelectorAll('.menu ul li');
+    
+    const a = {
+        '/': 0,
+        '/index.html': 0,
+        '/daotao.html': 1,
+        '/sukien.html': 2,
+        '/thanhtuu.html': 3
+    }
+
+    if (a[window.location.pathname] !== undefined) {
+        menuItems[a[window.location.pathname]].classList.add('active');
+    }
+}
+
+function initFooter(){
+    const footer = document.querySelector('footer');
+    footer.innerHTML = footerHTML;
+}
+
